fbd391f1685b69408f934f8cf2ad503ba99c8858
sutavinvinothan <svinotha@uwo.ca>
2025-02-27 22:06:48 -0500

    initial commit

---
+++ Nectar/Nectar/NectarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NectarApp.swift
+//  Nectar
+//
+//  Created by Sutavin Vinothan on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct NectarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nectar/NectarTests/NectarTests.swift
@@ -0,0 +1,16 @@
+//
+//  NectarTests.swift
+//  NectarTests
+//
+//  Created by Sutavin Vinothan on 2025-02-27.
+//
+
+import Testing
+
+struct NectarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
